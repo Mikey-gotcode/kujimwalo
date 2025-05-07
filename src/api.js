155760c0c0d7e137e8348ecc146eacf59017d080
Mikey-gotcode <michael.irungu@strathmore.edu>
2025-05-07 14:57:20 +0300

    vps ip

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export default {
-    baseURL: 'https://streets-specialty-webmasters-promotional.trycloudflare.com/api',
-    qrURL:'https://equally-cheapest-retrieve-accounting.trycloudflare.com',
+    baseURL: 'http://178.32.229.74/api',
+    qrURL:'http://178.32.229.74',
     localURL:'http://127.0.0.1:8000/api'
   };
   
