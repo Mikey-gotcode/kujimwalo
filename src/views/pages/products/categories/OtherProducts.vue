fdc05c697972002df0fcd83aeb0b74c841284e0e
Mikey-gotcode <michael.irungu@strathmore.edu>
2025-05-08 16:18:25 +0300

    /

--- src/views/pages/products/categories/OtherProducts.vue
+++ src/views/pages/products/categories/OtherProducts.vue
@@ -31,7 +31,7 @@ const activeImageIndex = ref({});
 function getImageUrl(product) {
   if (!product.images.length) return '/default-image.jpg';
   const idx = activeImageIndex.value[product.id] || 0;
-  return `${api.defaults.baseURL}/${product.images[idx].image_path}`;
+  return `${api.defaults.baseURL}${product.images[idx].image_path}`;
 }
 
 
